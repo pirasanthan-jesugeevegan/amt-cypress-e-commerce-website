02e463f978f1f97cc6dce6595cd942a090e9de02
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2020-08-06 17:46:53 +0100

    report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,13 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from 'mochawesome/addContext'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.on("test:after:run", (test, runnable) => {
+
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
